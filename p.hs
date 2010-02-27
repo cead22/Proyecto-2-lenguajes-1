2b7231efc32340cbb893a486b829aebbc8851462
Carlos <cead22@gmail.com>
2010-02-27 11:47:29 -0430

    suma mejorada pero sin recursion de cola

--- p.hs
+++ p.hs
@@ -11,32 +11,43 @@ parteFraccionaria [] base n = 0
 parteFraccionaria (x:xs) base n = (fromIntegral x :: Double)*(1/(fromIntegral base^n :: Double ))
 		  	      	  + parteFraccionaria xs base (n+1)
 
-showRA::RealArbitrario -> String
-showRA (NoNeg x y base) = show ((fromIntegral (parteEntera x base 0) :: Double) + (parteFraccionaria y base 1))
-showRA (Neg x y base) = show (negate ((fromIntegral (parteEntera x base 0) :: Double) + (parteFraccionaria y base 1)))
-
-adicion::[Int] -> [Int] -> Int -> [Int]
-adicion [] x base = x
-adicion x [] base = x
-adicion (x:xs) (y:ys) base = if x + y >= base
-	       	      	     then 
-			     	  if xs == []
-			     	  then [x + y - base] ++ (adicion [1] ys base)
-				  else [x + y - base] ++ (adicion ([(head xs) + 1] ++ (tail xs)) ys base)
-			     else [x + y] ++ (adicion xs ys base)
-
 convertir::RealArbitrario -> RealArbitrario
 convertir (NoNeg x y base) = Neg x y base
 convertir (Neg x y base) = NoNeg x y base
 
-sumaRA::RealArbitrario -> RealArbitrario -> RealArbitrario
-sumaRA (NoNeg (x:xs) (y:ys) base1) (NoNeg (z:zs) (w:ws) base2) = --if base1 /= base2
-								 --then throw "Error bases distintas"
-								 --else 
-								      let f = adicion (reverse (y:ys)) (reverse (w:ws)) base1
-								      in
-									if length f > max (length (y:ys)) (length (w:ws))
-									then (NoNeg (adicion ([x + 1] ++ xs) (z:zs) base1) (tail (reverse f)) base1)
-									else (NoNeg (adicion (x:xs) (z:zs) base1) f base1)
+showRA::RealArbitrario -> String
+showRA (NoNeg x y base) = show ((fromIntegral (parteEntera x base 0) :: Double) + (parteFraccionaria y base 1))
+showRA (Neg x y base) = show (negate ((fromIntegral (parteEntera x base 0) :: Double) + (parteFraccionaria y base 1)))
 
+acarreo::Int -> Int -> Int
+acarreo s base 
+	  | s >= base = 1
+	  | otherwise = 0
+
+adicionEnt::[Int] -> [Int] -> Int -> Int -> [Int]
+adicionEnt [] x base ac = x
+adicionEnt x [] base ac = x
+adicionEnt (x:xs) (y:ys) base ac = 
+				[(x + y + ac) `mod` base] ++ (adicionEnt xs ys base c)
+			     	where c = acarreo (x + ac + y) base
+
+adicionFrac::[Int] -> [Int] -> Int -> ([Int],Int)
+adicionFrac [] x base = (x,0)
+adicionFrac x [] base = (x,0)
+adicionFrac (x:xs) (y:ys) base = 
+	    	   	       ([(x + y + ac) `mod` base] ++ l, acarreo (x + y) base)
+			       where (l,ac) = (adicionFrac xs ys base)
+			     	       
+
+sumaRA::RealArbitrario -> RealArbitrario -> RealArbitrario
+sumaRA (NoNeg x1 y1 base1) (NoNeg x2 y2 base2) = 
+       	      	     	    	   	  	(NoNeg x y base1)
+						where {
+						      (y,z) = adicionFrac y1 y2 base1
+						      ;x = adicionEnt x1 x2 base1 z
+						      }
+
+{-
 sumaRA (Neg (x:xs) (y:ys) base1) (Neg (z:zs) (w:ws) base2) = convertir (sumaRA (NoNeg (x:xs) (y:ys) base1) (NoNeg (z:zs) (w:ws) base2))
+
+-}
