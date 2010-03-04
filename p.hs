979019dff62ac67fe3802a59164827277d65bbf7
liliana <liliana@leia.(none)>
2010-03-04 15:29:17 -0430

    con cometarios a medias XD

--- p.hs
+++ p.hs
@@ -1,33 +1,67 @@
 data RealArbitrario = NoNeg [Int] [Int] Int
      		    | Neg [Int] [Int] Int
 		      deriving (Eq)
-
-parteEntera ::[Int] -> Int -> Int -> Int
-parteEntera [] base n = 0
-parteEntera (x:xs) base n = x * (base^n) + parteEntera xs base (n + 1)
-
-parteFraccionaria::[Int] -> Int -> Int -> Double
-parteFraccionaria [] base n = 0
-parteFraccionaria (x:xs) base n = (fromIntegral x :: Double) * (1 / (fromIntegral base^n :: Double ))
-		  	      	  + parteFraccionaria xs base (n+1)
-
+------------------ convertir ----------------------------------
+-- Transforma un RealArbitrario en su opuesto (Mismo numero
+-- con diferente signo)
+--
+-- @param RealArbitrario : RealArbitrario al cual se
+-- le desea cambiar el signo.
+--
+-- Retorna: RealArbitrario con signo opuesto
+----------------------------------------------------------------
 convertir::RealArbitrario -> RealArbitrario
 convertir (NoNeg x y base) = Neg x y base
 convertir (Neg x y base) = NoNeg x y base
 
+----------------- compararBase -----------------------------------
+-- Determina si dos bases son iguales.
+--
+-- @param Int: base1
+-- @param Int base 2
+--
+--Retorna : True si las bases son iguales False en caso contario.
+-----------------------------------------------------------------
 compararBase::Int -> Int -> Bool
 compararBase base1 base2 = 
 	if base1 == base2 then True else False
+-------------------- acarreoSum --------------------------
+-- Retorna un string que muestra un RealArbitrario en base 10.
+--
+-- @param RealArbitrario : parametro a transformar
+--
+-- Retorna : String que representa el RealArbitrario.
+-----------------------------------------------------------
 
 showRA::RealArbitrario -> String
 showRA (NoNeg x y base) = show ((fromIntegral (parteEntera x base 0) :: Double) + (parteFraccionaria y base 1))
 showRA (Neg x y base) = show (negate ((fromIntegral (parteEntera x base 0) :: Double) + (parteFraccionaria y base 1)))
 
+-------------------- acarreoSum --------------------------
+-- Determina si hay o no acarreo en la suma y retorna su valor.
+--
+-- @param Int : Numero que permite determina si hay que llevar
+-- acarreo o no.
+-- @param Int : Base en la que se encuentra el Int dado.
+--
+-- Retorna : Acarreo resultante
+-----------------------------------------------------------
 acarreoSum::Int -> Int -> Int
 acarreoSum s base 
     | s >= base = 1
     | otherwise = 0
 
+
+------------------ adicionEnt ----------------------------------
+-- Calcula la suma de dos listas de enteros de la misma base
+--
+-- @param [Int] : primera lista a sumar (en orden inverso)
+-- @param [Int] : segunda lista a sumar (en orden inverso)
+-- @param Int : base de las listas.
+-- @param Int : acarreo que lleva la suma.
+--
+-- Retorna: Lista con el resultado de la suma (en orden inverso)
+-----------------------------------------------------------------
 adicionEnt::[Int] -> [Int] -> Int -> Int -> [Int]
 adicionEnt [] [] base ac = [ac]
 adicionEnt [] (x:xs) base ac = adicionEnt (x:xs) [ac] base 0
@@ -36,8 +70,15 @@ adicionEnt (x:xs) (y:ys) base ac =
     [(x + y + ac) `mod` base] ++ (adicionEnt xs ys base c)
 	where c = acarreoSum (x + ac + y) base
 
+----------------------- sumaRA ----------------------------------
+-- Calcula la suma de dos reales arbitrarios
+--
+-- @param RealArbitrario : primer parametro sumar
+-- @param RealArbitrario : segundo parametro a sumar
+--
+-- Retorna: RealArbitrario resultante de la suma.
+-----------------------------------------------------------------
 sumaRA::RealArbitrario -> RealArbitrario -> RealArbitrario
-
 sumaRA (NoNeg x1 y1 base1) (NoNeg x2 y2 base2) = 
     if compararBase base1 base2
     	then  NoNeg y (reverse x) base1
@@ -59,16 +100,43 @@ sumaRA (NoNeg (x:xs) (y:ys) base1) (Neg (z:zs) (w:ws) base2) =
 sumaRA (Neg (x:xs) (y:ys) base1) (NoNeg (z:zs) (w:ws) base2) = 
     restaRA (NoNeg (x:xs) (y:ys) base1) (NoNeg (z:zs) (w:ws) base2)
 
+----------------------- limpia ----------------------------------
+-- Quita todos los 0s no significativos de un numero con representacion
+-- de lista.
+--
+-- @param [Int] : Representacion del numero a tratar
+--
+-- Retorna: Lista equivanlente a la pasada como parametro pero sin
+-- los ceros no significativos
+-----------------------------------------------------------------
 limpia::[Int] -> [Int]
 limpia [] = []
 limpia (x:xs) = if x == 0
                 then limpia xs
                 else (x:xs)
-          
+
+----------------------- limpiar ----------------------------------
+-- Quita todos los 0s no significativos de un RealAribtrario
+--
+-- @param RealArbitrario : Representacion del numero a tratar
+--
+-- Retorna: Lista equivanlente a la pasada como parametro pero sin
+-- los ceros no significativos
+-----------------------------------------------------------------          
 limpiar::RealArbitrario -> RealArbitrario
 limpiar (NoNeg x y base) = NoNeg (reverse (limpia (reverse x))) (reverse (limpia (reverse y))) base
 
--- recibe dos listas del mismo tamano, al derecho
+
+-------------------mayorEstrictoporNumero -----------------------
+-- Toma dos listas de igual tamano y determina si la primera
+-- representa un numero mayor que el representado por la
+-- segunda lista.
+--
+-- @param [Int] : Primera lista a comparar (en Orden)
+-- @param [Int] : Segunda lista a comparar (en Orden)
+--
+-- Retorna: True si se cumple la condicion False en caso contrario
+-----------------------------------------------------------------
 mayorEstrictoPorNumero::[Int] -> [Int] -> Bool
 mayorEstrictoPorNumero [] [] = False
 mayorEstrictoPorNumero (x:xs) [] = True
@@ -78,24 +146,60 @@ mayorEstrictoPorNumero (x:xs) (y:ys)
     | x < y = False
     | otherwise = mayorEstrictoPorNumero xs ys
 
--- recibe listas al derecho y limpias
+-------------------mayorEstrictoporNumero -----------------------
+-- Toma dos listas en orden y sin 0s no significativos.Y determina
+-- si la primera representa un numero mayor que el representado
+-- por la segunda lista.
+--
+-- @param [Int] : Primera lista a comparar (en Orden)
+-- @param [Int] : Segunda lista a comparar (en Orden)
+--
+-- Retorna: True si se cumple la condicion False en caso contrario
+-----------------------------------------------------------------
 mayorEstricto::[Int] -> [Int] -> Bool
 mayorEstricto x y
     | length x > length y = True
     | length x == length y = mayorEstrictoPorNumero x y
     | otherwise = False
 
+---------------------- mayor ------------------------------------
+-- Toma dos RealArbitrario y determina si el primero es mayor
+-- que el segundo
+--
+-- @param RealArbitrario : Primera parametro a comparar
+-- @param RealArbitrario : Segunda parametro lista a comparar
+--
+-- Retorna: True si se cumple la condicion False en caso contrario
+-----------------------------------------------------------------
 mayor::RealArbitrario -> RealArbitrario -> Bool
 mayor (NoNeg x1 y1 base1) (NoNeg x2 y2 base2)
     | mayorEstricto (reverse x1) (reverse x2) = True
     | reverse x1 == reverse x2 &&  y1 >= y2 = True
     | otherwise = False
 
+-------------------- prestamo --------------------------
+-- Determina si hay o no acarreo en la resta y retorna su valor.
+--
+-- @param Int : Numero que permite determina si hay que llevar
+-- acarreo o no.
+--
+-- Retorna : Acarreo resultante
+-----------------------------------------------------------
 prestamo::Int -> Int
 prestamo s
     | s < 0 = 1
     | otherwise = 0
 
+------------------ sustracionEnt --------------------------------
+-- Calcula la resta de dos listas de enteros de la misma base
+--
+-- @param [Int] : primera lista (en orden inverso)
+-- @param [Int] : segunda lista  (en orden inverso)
+-- @param Int : base de las listas.
+-- @param Int : acarreo que lleva la resta.
+--
+-- Retorna: Lista con el resultado de la resta (en orden)
+-----------------------------------------------------------------
 sustraccionEnt::[Int] -> [Int] -> Int -> Int -> [Int]
 sustraccionEnt [] [] base pr = []
 sustraccionEnt [] (x:xs) base pr = [x - pr] ++ xs
@@ -104,6 +208,15 @@ sustraccionEnt (x:xs) (y:ys) base pr =
     [(x - y - pr) `mod` base] ++ (sustraccionEnt xs ys base c)
 	where c = prestamo (x - y - pr)
 
+----------------------- restaRA ----------------------------------
+-- Calcula la suma de dos reales arbitrarios. El primer paramatro
+-- debe ser mayor que el segundo.
+--
+-- @param RealArbitrario : primer parametro restar
+-- @param RealArbitrario : segundo parametro a restar
+--
+-- Retorna: RealArbitrario resultante de la resta
+-----------------------------------------------------------------
 restaR::RealArbitrario -> RealArbitrario -> RealArbitrario
 restaR (NoNeg x1 y1 base1) (NoNeg x2 y2 base2) = 
     if compararBase base1 base2
@@ -116,7 +229,16 @@ restaR (NoNeg x1 y1 base1) (NoNeg x2 y2 base2) =
       tam_1 = length y1 ;
       tam_2 = length y2
     }
-
+----------------------- restaRA ----------------------------------
+-- Calcula la suma de dos reales arbitrarios, en caso de que el
+-- primer argumento sea menor que el segundo calcula la resta 
+-- opuesta (Resultado equivalente).
+--
+-- @param RealArbitrario : primer parametro restar
+-- @param RealArbitrario : segundo parametro a restar
+--
+-- Retorna: RealArbitrario resultante de la resta
+-----------------------------------------------------------------
 restaRA::RealArbitrario -> RealArbitrario -> RealArbitrario
 restaRA (NoNeg x1 y1 base1) (NoNeg x2 y2 base2)
     | (mayor num1 num2) = restaR num1 num2
@@ -130,19 +252,48 @@ restaRA (NoNeg x1 y1 base1) (Neg x2 y2 base2) = sumaRA (NoNeg x1 y1 base1) (NoNe
 restaRA (Neg x1 y1 base1) (NoNeg x2 y2 base2) = convertir (sumaRA (NoNeg x1 y1 base1) (NoNeg x2 y2 base2))
 
 
-
+-------------------- acarreoMult--------------------------
+-- Determina si hay o no acarreo en la muitiplicacion y retorna
+-- su valor
+--
+-- @param Int : Numero que permite determina si hay que llevar
+-- acarreo o no.
+-- @param Int : Base en la que se encuentra el Int dado.
+--
+-- Retorna : Acarreo resultante
+-----------------------------------------------------------
 acarreoMult::Int -> Int -> Int
 acarreoMult s base
     | s >= base = s `div` base
     | otherwise = 0
 
+------------------ multiNumEnt --------------------------------
+-- Calcula la multiplicacion de un numero representado en una lista
+-- con un numero entero 
+--
+-- @param [Int] : lista a multiplicar (en orden)
+-- @param Int : entero por el cual se desea multiplicar
+-- @param Int : base de los numero.
+-- @param Int : acarreo que lleva la multiplicacion.
+--
+-- Retorna: Lista con el resultado de la multiplicacion(en orden)
+-----------------------------------------------------------------
 multiNumEnt::[Int] -> Int -> Int -> Int -> [Int]
 multiNumEnt [] n base ac = [ac]
 multiNumEnt (x:xs) n base ac = 
     [(x * n + ac) `mod` base] ++ (multiNumEnt xs n base c)
     where c = acarreoMult (x * n + ac) base
 
--- recibe listas al derecho
+------------------ multiNumEnt --------------------------------
+-- Calcula la multiplicacion de dos numero con representacion en listas 
+--
+-- @param [Int] : lista a multiplicar (en orden)
+-- @param [Int] : lista por la que se multiplica multiplicar (en orden)
+-- @param Int : base de los numero.
+-- @param Int : acarreo que lleva la multiplicacion.
+--
+-- Retorna: Lista con el resultado de la multiplicacion(en orden inverso)
+-----------------------------------------------------------------
 multi::[Int] -> [Int] -> Int -> Int -> [Int]
 multi x [] base c = []
 multi x (y:ys) base c =
@@ -152,6 +303,14 @@ multi x (y:ys) base c =
       b = multi x (init (y:ys)) base (c + 1)
     }
 
+----------------------- multRA ----------------------------------
+-- Calcula la multiplicacion de dos reales arbitrarios.
+--
+-- @param RealArbitrario : primer parametro de la mutiplicacion
+-- @param RealArbitrario : segundo parametro de la multiplicacion
+--
+-- Retorna: RealArbitrario resultante de la multiplicacion
+-----------------------------------------------------------------
 multRA::RealArbitrario -> RealArbitrario -> RealArbitrario
 multRA (NoNeg x1 y1 base1) (NoNeg x2 y2 base2) =
     if compararBase base1 base2
@@ -166,6 +325,17 @@ multRA (Neg x1 y1 base1) (Neg x2 y2 base2) = multRA (NoNeg x1 y1 base1) (NoNeg x
 multRA (NoNeg x1 y1 base1) (Neg x2 y2 base2) = convertir (multRA (NoNeg x1 y1 base1) (NoNeg x2 y2 base2))
 multRA (Neg x1 y1 base1) (NoNeg x2 y2 base2) = convertir (multRA (NoNeg x1 y1 base1) (NoNeg x2 y2 base2))
 
+------------------ divSimple --------------------------------
+-- Calcula la division de un numero representado en una lista
+-- con un numero entero 
+--
+-- @param [Int] : lista a dividir (en orden)
+-- @param Int : divisor
+-- @param Int : base de los numero.
+-- @param Int : resto.
+--
+-- Retorna: Lista con el resultado de la division(en orden)
+-----------------------------------------------------------------
 divSimple::[Int] -> Int -> Int -> Int -> ([Int],Int)
 divSimple [] n base r = ([],r)
 divSimple (x:xs) n base r =
@@ -175,13 +345,30 @@ divSimple (x:xs) n base r =
       rem = (r*base + x) `mod` n ;
       (a,b) = (divSimple xs n base rem)
     }
-
+-------------------- prestamoDiv--------------------------
+-- Determina si hay o no prestamo en la division y retorna
+-- su valor
+--
+-- @param Int : Numero que permite determina si hay que pedir
+--prestamo
+-- @param Int : Base en la que se encuentra el Int dado.
+--
+-- Retorna : prestamo resultante.
+-----------------------------------------------------------
 prestamoDiv::Int -> Int -> Int
 prestamoDiv x y
     | x < y = 1
     | otherwise = 0
 
--- recibe listas al reves
+------------------------ divaux --------------------------------
+-- Calcula la division de dos numero con representacion en listas 
+--
+-- @param [Int] : lista dividendo (en orden inverso)
+-- @param [Int] : lista divisor (en orden inverso)
+-- @param Int : base de los numero.
+-- @param Int : prestamo
+--
+-- Retorna: Lista con el resultado de la division (en orden inverso)
 divaux::[Int] -> [Int] -> Int -> Int -> ([Int],[Int])
 divaux [] (y:ys) base n = ([],[0])
 divaux (x:xs) (y:ys) base n =
@@ -196,7 +383,17 @@ divaux (x:xs) (y:ys) base n =
               prueba = reverse(limpia(reverse(multiNumEnt (y:ys) n base 0)))
             }
 
-
+---------------------------- div1 --------------------------------
+-- Calcula la division de dos numero con representacion en listas 
+--
+-- @param [Int] : lista dividendo (en orden inverso)
+-- @param [Int] : lista divisor (en orden inverso)
+-- @param Int : base de los numero.
+-- @param Int : prestamo
+-- @parame [Int] : resultado de la division
+--
+-- Retorna: Lista con el resultado de la division (en orden) y una lista
+-- con el modulo de la division. 
 div1::[Int] -> [Int] -> Int -> Int -> [Int] -> ([Int],[Int])
 div1 [] (y:ys) base i res = (res,[0])
 div1 [0] (y:ys) base i res = (res++[0],[0])
@@ -215,6 +412,18 @@ div1 (x:xs) (y:ys) base i res =
              (resultado,modulo) = divaux (reverse (x1)) (reverse (y:ys)) base (base-1)
            }
 
+---------------------------- div2 --------------------------------
+-- Calcula la division de dos numero con representacion en listas,
+-- anadiendo 0 al resultado cuando sea necesario. 
+--
+-- @param [Int] : lista dividendo (en orden inverso)
+-- @param [Int] : lista divisor (en orden inverso)
+-- @param Int : base de los numero.
+-- @param Int : prestamo
+-- @parame [Int] : resultado de la division
+--
+-- Retorna: Lista con el resultado de la division (en orden) y una lista
+-- con el modulo de la division. 
 div2::[Int] -> [Int]-> Int -> Int -> [Int] -> ([Int],[Int])
 div2 [] (y:ys) base i res = (res,[0])
 div2 [0] (y:ys) base i res = (res++[0],[0])
@@ -242,6 +451,14 @@ divConDecimales x y base decimales =
       (frac,modfrac) = div2 (modent ++ [ 0 | j <- [1..decimales]]) y base ((length modent))  []
     }
 
+----------------------- divRA ----------------------------------
+-- Calcula la division de dos reales arbitrarios.
+--
+-- @param RealArbitrario : divisor
+-- @param RealArbitrario : dividendo
+--
+-- Retorna: RealArbitrario resultante de la division
+-----------------------------------------------------------------
 divRA::RealArbitrario -> RealArbitrario -> Int -> RealArbitrario
 divRA (NoNeg x1 y1 base1) (NoNeg x2 y2 base2) decimales =
     if compararBase base1 base2
